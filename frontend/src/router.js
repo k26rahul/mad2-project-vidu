bea510504f6b046379399992fc299d7c348202f4
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-03-11 10:43:02 +0530

    created auth routes in router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,7 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import IndexView from './views/IndexView.vue';
+import LoginView from './views/auth/LoginView.vue';
+import CustomerRegisterView from './views/auth/CustomerRegisterView.vue';
+import ProfessionalsRegisterView from './views/auth/ProfessionalsRegisterView.vue';
 
-const routes = [{ path: '/', component: IndexView }];
+const routes = [
+  { path: '/', component: IndexView },
+  { path: '/login', component: LoginView },
+  { path: '/register/customer', component: CustomerRegisterView },
+  { path: '/register/professionals', component: ProfessionalsRegisterView },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
--- frontend/src/views/IndexView.vue
+++ frontend/src/views/IndexView.vue
@@ -1,3 +1,16 @@
 <template>
+  <nav>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/register/customer">Register as Customer</router-link>
+    <router-link to="/register/professionals">Register as Professionals</router-link>
+  </nav>
   <h1>Household Services v2</h1>
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 1rem;
+  margin: 1rem 0;
+}
+</style>
