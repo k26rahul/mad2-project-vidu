a7f0a9fdbe8ecc4dc7c1248c4794faefbeec9e8c
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-03-11 16:11:11 +0530

    /api/login done

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from flask_security.utils import login_user, logout_user, verify_password
+from models import db, User
 
 auth_bp = Blueprint('auth_bp', __name__)
 bp = auth_bp
@@ -6,6 +8,22 @@ bp = auth_bp
 
 @bp.route('/api/login')
 def login():
+  email = request.form.get('email')
+  password = request.form.get('password')
+  user = User.query.filter_by(email=email).first()
+
+  if user and verify_password(password, user.password):
+    return jsonify({
+        "message": 'login successful'
+    })
+
+  return jsonify({
+      "message": 'email or password incorrect'
+  }), 401
+
+
+@bp.route('/api/logout')
+def logout():
   return 'OK'
 
 
