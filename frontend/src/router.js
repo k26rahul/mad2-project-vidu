6c9c9948e8ed36737f5a6c6a02a715b5e1b4baa3
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-03-11 10:11:38 +0530

    router setup done 💚

---
+++ frontend/src/router.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import IndexView from './views/IndexView.vue';
+
+const routes = [{ path: '/', component: IndexView }];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
